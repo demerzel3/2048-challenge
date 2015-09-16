341c63e422a32152a9a4a0f8ebf92fb2a1f06b41
Gabriele Genta <gabriele.genta@gmail.com>
2015-09-16 19:59:26 +0200

    Convert main application to angular component (sort of)

--- client/application.ts
+++ client/application.ts
@@ -1,9 +1,22 @@
+import {Component, View, bootstrap} from 'angular2/angular2';
 import {GameManager} from './game_manager';
 import {KeyboardInputManager} from './keyboard_input_manager';
 import {HTMLActuator} from './html_actuator';
 import {LocalStorageManager} from './local_storage_manager';
 
-// Wait till the browser is ready to render the game (avoids glitches)
-window.requestAnimationFrame(function () {
-    new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-});
+@Component({selector: 'application'})
+@View({templateUrl: '/client/view/index.ng.html'})
+class Application
+{
+    private gameManager;
+
+    constructor() {
+        // Wait till the browser is ready to render the game (avoids glitches)
+        window.requestAnimationFrame(() => {
+            this.gameManager = new GameManager(
+                4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+        });
+    }
+}
+
+bootstrap(Application);
