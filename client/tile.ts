8da8872db81975bc9ae164c59816b0c779e4739a
Gabriele Genta <gabriele.genta@gmail.com>
2015-09-19 11:51:52 +0200

    Refactor Tile to be a class

--- client/tile.ts
+++ client/tile.ts
@@ -1,27 +1,41 @@
-export function Tile(position, value) {
-    this.x = position.x;
-    this.y = position.y;
-    this.value = value || 2;
-
-    this.previousPosition = null;
-    this.mergedFrom = null; // Tracks tiles that merged together
+export interface Position {
+    x:number;
+    y:number;
 }
 
-Tile.prototype.savePosition = function () {
-    this.previousPosition = {x: this.x, y: this.y};
-};
+export class Tile {
+    public x:number;
+    public y:number;
+    public value:number;
 
-Tile.prototype.updatePosition = function (position) {
-    this.x = position.x;
-    this.y = position.y;
-};
+    public previousPosition:Position;
+    public mergedFrom:Tile;
 
-Tile.prototype.serialize = function () {
-    return {
-        position: {
-            x: this.x,
-            y: this.y
-        },
-        value: this.value
-    };
-};
+    constructor(position:Position, value:number) {
+        this.x = position.x;
+        this.y = position.y;
+        this.value = value || 2;
+
+        this.previousPosition = null;
+        this.mergedFrom = null; // Tracks tiles that merged together
+    }
+
+    public savePosition() {
+        this.previousPosition = {x: this.x, y: this.y};
+    }
+
+    public updatePosition(position:Position) {
+        this.x = position.x;
+        this.y = position.y;
+    }
+
+    public serialize() {
+        return {
+            position: {
+                x: this.x,
+                y: this.y
+            },
+            value: this.value
+        };
+    }
+}
